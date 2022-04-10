0975236d66992116753f40177bed3bc9352c801b
Markus Finell <finell.markus@gmail.com>
2022-04-10 21:26:28 +0200

    plugin updates

--- config/application.php
+++ config/application.php
@@ -110,7 +110,7 @@ Config::define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?: true);
  */
 Config::define('WP_DEBUG_DISPLAY', false);
 Config::define('WP_DEBUG_LOG', false);
-Config::define('SCRIPT_DEBUG', false);
+Config::define('SCRIPT_DEBUG', true);
 ini_set('display_errors', '0');
 
 /**
